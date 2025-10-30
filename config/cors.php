7a6df4c048d3be4fb80fb3a140cd1ffcb217348e
Alassane <asdoussynou66@gmail.com>
2025-10-30 03:04:00 +0000

    autorisation

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*'], // Keep * for development, but consider specifying origins in production
 
     'allowed_origins_patterns' => [],
 
